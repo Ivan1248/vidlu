1adc11bf4bb4cee5e7302fbe75a3049b833ccff5
Ivan <ivan.grubisic.mail@gmail.com>
2019-06-23 20:33:25 +0200

    FIxed ResNet initialization for ResNet V1

--- vidlu/training/initialization.py
+++ vidlu/training/initialization.py
@@ -19,11 +19,12 @@ def kaiming_resnet(module, nonlinearity='relu', zero_init_residual=True):
     if zero_init_residual:
         found = False
         for m in module.modules():
-            if isinstance(m, com.ResNetV2Unit):
-                for i, c in enumerate(reversed(list(m.children()))):
-                    if i == 1 and isinstance(c, mod.BatchNorm):
-                        found = True
+            if isinstance(m, (com.ResNetV2Unit, com.ResNetV1Unit)):
+                for i, c in enumerate(reversed(list(m.modules()))):
+                    if isinstance(c, mod.BatchNorm):
                         nn.init.constant_(c.orig.weight, 0)
+                        found = True
+                        break
         if not found:
             warnings.warn("Batch normalization modules for residual zero-init not found.")
 
@@ -32,7 +33,7 @@ def kaiming_densenet(module, nonlinearity='relu'):
     # from torchvision/models/densenet.py
     for m in module.modules():
         if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, nonlinearity=nonlinearity)  # added nonlinearity
+            nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity=nonlinearity)  # added nonlinearity
         elif isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
             nn.init.constant_(m.bias, 0)
