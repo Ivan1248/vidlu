2fa6b643f393ef600832cc91e06f74af55572aea
Ivan <ivan.grubisic.mail@gmail.com>
2019-07-22 21:41:02 +0200

    Implemented problem classes for use in factories instead of the dataset

--- vidlu/problem.py
+++ vidlu/problem.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 
 
@@ -6,3 +7,39 @@ class Problem(Enum):
     SEMANTIC_SEGMENTATION = 'semantic_segmentation'
     DEPTH_REGRESSION = 'depth_regression'
     OTHER = 'other'
+
+
+@dataclass
+class ProblemInfo:
+    pass
+
+
+@dataclass
+class Supervised(ProblemInfo):
+    class_count: int
+
+
+@dataclass
+class Classification(Supervised):
+    class_count: int
+
+
+@dataclass
+class SemanticSegmentation(Classification):
+    y_shape: tuple
+
+
+@dataclass
+class DepthRegression(Supervised):
+    y_shape: tuple
+
+
+_name_to_class = dict(classification=Classification, semantic_segmentation=SemanticSegmentation)
+
+
+def get_problem_type(name: str):
+    return _name_to_class[name]
+
+
+def get_problem(name: str, *args, **kwargs):
+    return get_problem_type(name)(*args, **kwargs)
