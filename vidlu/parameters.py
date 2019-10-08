794d8807f4acdf1bbfe2ff172afd515a237359f4
Ivan <ivan.grubisic.mail@gmail.com>
2019-10-08 22:04:40 +0200

    Make parameter translation more flexible from

---
+++ vidlu/parameters.py
@@ -0,0 +1,116 @@
+from collections import Mapping
+
+import torch
+
+from vidlu.utils import text
+
+
+def translate_dict_keys(dict_, input_output_format_pairs):
+    translator = text.FormatTranslatorCascade(input_output_format_pairs)
+    return {translator(k): v for k, v in dict_.items()}
+
+
+def get_translated_parameters(translator_name, state_dict_or_path, subdict=''):
+    state_dict = (state_dict_or_path if isinstance(state_dict_or_path, Mapping)
+                  else torch.load(state_dict_or_path))
+    subdict = [] if not subdict else subdict.split(',')
+    for sub in subdict:
+        state_dict = state_dict[sub]
+    return translate(translator_name, state_dict)
+
+
+def translate(translator_name, state_dict):
+    return globals()[f"translate_{translator_name}"](state_dict)
+
+
+# Translators ##################################################################
+
+
+def translate_id(state_dict):
+    return state_dict
+
+
+def translate_madrylab_resnet(state_dict):
+    start = 'module.model.'
+    state_dict = {k[len(start):]: v for k, v in state_dict['model'].items() if k.startswith(start)}
+    return translate_resnet(state_dict)
+
+
+def translate_resnet(state_dict):
+    return translate_dict_keys(
+        state_dict,
+        {  # backbone
+            r"{a:conv|bn}1.{e}":
+                r"backbone.root.{a:bn->norm}.orig.{e}",
+            r"layer{a:(\d+)}.{b:(\d+)}.{c:conv|bn}{d:(\d+)}.{e}":
+                r"backbone.bulk.unit{`int(a)-1`}_{b}.fork.block.{c:bn->norm}{`int(d)-1`}.orig.{e}",
+            r"layer{a:(\d+)}.{b:(\d+)}.downsample.{c:0|1}.{e:(.*)}":
+                r"backbone.bulk.unit{`int(a)-1`}_{b}.fork.shortcut.{c:0->conv|1->norm}.orig.{e}",
+            # logits
+            r"fc.{e:(.*)}":
+                r"head.logits.orig.{e}"
+        }.items())
+
+
+def translate_densenet(state_dict):
+    state_dict = translate_dict_keys(
+        state_dict,
+        {  # backbone
+            r"bulk.{a:conv|norm}0.{e:(.*)}":
+                r"backbone.root.{a}.orig.{e}",
+            r"bulk.denseblock{a:(\d+)}.denselayer{b:(\d+)}.{c:conv|norm}{d:(\d+)}.{e}":
+                r"backbone.bulk.dense_block{`int(a)-1`}.unit{`int(b)-1`}.block.{c}{`int(d)-1`}.orig.{e}",
+            r"bulk.transition{a:(\d+)}.{b:conv|norm}.{e}":
+                r"backbone.bulk.transition{`int(a)-1`}.{b}.orig.{e}",
+            r"bulk.norm(\d+).{e:(.*)}":  # the number matching the (\d+) (1+number of blocks) is ignored
+                r"backbone.bulk.norm.orig.{e}",
+            # logits
+            r"classifier.{e:(.*)}":
+                r"head.logits.orig.{e}"
+        }.items())
+    return state_dict
+
+
+def translate_swiftnet(state_dict):
+    state_dict = translate_dict_keys(
+        state_dict,
+        {  # backbone
+            r"backbone.{a:conv|bn}1{e}":
+                r"backbone.backbone.root.{a:bn->norm}.orig{e}",
+            r"backbone.layer{a:(\d+)}.{b:(\d+)}.{c:conv|bn}{d:(\d+)}.{e}":
+                r"backbone.backbone.bulk.unit{`int(a)-1`}_{b}.fork.block.{c:bn->norm}{`int(d)-1`}.orig.{e}",
+            r"backbone.layer{a:(\d+)}.{b:(\d+)}.downsample.{c:0|1}.{e:(.*)}":
+                r"backbone.backbone.bulk.unit{`int(a)-1`}_{b}.fork.shortcut.{c:0->conv|1->norm}.orig.{e}",
+            # spp
+            r"backbone.spp.spp.{a:spp_bn|spp_fuse}.{b:norm|conv}.{e:(.*)}":
+                r"backbone.context.{a:spp_bn->input_block|spp_fuse->fuse_block}.{b}0.orig.{e}",
+            r"backbone.spp.spp.spp{a:(\d+)}.{b:norm|conv}.{e:(.*)}":
+                r"backbone.context.pyramid.block{a}.{b}0.orig.{e}",
+            # ladder
+            r"backbone.upsample.{a:(\d+)}.{b:bottleneck|blend_conv}.{c:norm|conv}.{e:(.*)}":
+                r"backbone.ladder.up_blends.{a}.{b:bottleneck->project|blend_conv->blend}.{c}0.orig.{e}",
+            r"logits.norm.{e:(.*)}":
+                r"backbone.norm.orig.{e}",
+            # logits
+            r"logits.conv.{e:(.*)}":
+                r"head.logits.orig.{e}"
+        }.items())
+    class_count = state_dict["head.logits.orig.weight"].shape[0]
+    # requires_grad=False is ignored
+    state_dict["head.logits.orig.bias"] = torch.zeros(class_count)
+    return state_dict
+
+
+def translate_swiftnet_orig_backbone(state_dict):
+    state_dict = translate_dict_keys(
+        state_dict,
+        {  # logits
+            r"logits.conv{e:(.*)}": "head.logits.orig{e}",
+            # everything else
+            r"logits.norm{e:(.*)}": "backbone.norm{e}",
+            r"backbone.{e:(.*)}": "backbone.{e}"
+        }.items())
+    class_count = state_dict["head.logits.orig.weight"].shape[0]
+    # requires_grad=False is ignored
+    state_dict["head.logits.orig.bias"] = torch.zeros(class_count)
+    return state_dict
