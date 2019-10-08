70485ac7580994ba753f21e8fc81403b2320d194
Ivan <ivan.grubisic.mail@gmail.com>
2019-10-08 21:02:37 +0200

    Add comment

--- scripts/set_cuda_order_pci.py
+++ scripts/set_cuda_order_pci.py
@@ -5,4 +5,4 @@ for modulename in ['torch', 'tensorflow', 'mxnet', 'chainer']:
     if modulename in sys.modules:
         raise RuntimeError(f"{modulename} should be imported after {__name__}.")
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # as opposed to "FASTEST_FIRST"
