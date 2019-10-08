4724543042bf6627bf19d028bf737f529a707621
Ivan <ivan.grubisic.mail@gmail.com>
2019-10-08 22:23:56 +0200

    Add random_labels parameter for CIFAR

--- vidlu/data/datasets/datasets.py
+++ vidlu/data/datasets/datasets.py
@@ -4,6 +4,7 @@ import tarfile
 import gzip
 import zipfile
 from pathlib import Path
+import os
 import shutil
 import warnings
 
@@ -19,6 +20,10 @@ from vidlu.utils.misc import download
 from vidlu.transforms import numpy as numpy_transforms
 
 from ._cityscapes_labels import labels as cslabels
+
+# Constants
+
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
 
 
 # Helper functions
@@ -262,7 +267,8 @@ class Cifar10(Dataset):
             tar.extractall(path=datasets_dir)
         download_path.unlink()
 
-    def __init__(self, data_dir, subset='trainval'):
+    def __init__(self, data_dir, subset='trainval', random_labels=False):
+        self.random_labels = random_labels
         _check_subsets(self.__class__, subset)
         data_dir = Path(data_dir)
 
@@ -292,10 +298,13 @@ class Cifar10(Dataset):
             self.x, self.y = test_x, test_y
         else:
             raise ValueError("The value of subset must be in {'train','test'}.")
-        super().__init__(subset=subset, info=dict(class_count=10, problem='classification'))
+        super().__init__(subset=subset, info=dict(class_count=10, problem='classification'), modifiers=['random_labels'] if self.random_labels else None)
 
     def get_example(self, idx):
-        return _make_record(x=self.x[idx], y=self.y[idx])
+        if self.random_labels:
+            return _make_record(x=self.x[idx], y=self.y[(idx+1) % len(self)])
+        else:
+            return _make_record(x=self.x[idx], y=self.y[idx])
 
     def __len__(self):
         return len(self.x)
@@ -484,6 +493,174 @@ class TinyImages(Dataset):
 
     def __len__(self):
         return 79302017
+
+
+def make_dataset(dataset_dir, class_to_idx, extensions=None, is_valid_file=None):
+    images = []
+    dataset_dir = Path(dir)
+    if not ((extensions is None) ^ (is_valid_file is None)):
+        raise ValueError(
+            "Both extensions and is_valid_file cannot be None or not None at the same time")
+    if extensions is not None:
+        def is_valid_file(x):
+            return x.lower().endswith(extensions)
+    for target in sorted(class_to_idx.keys()):
+        d = dataset_dir / target
+        if not d.is_dir():
+            continue
+        for root, _, fnames in sorted(os.walk(d)):
+            for fname in sorted(fnames):
+                path = Path(root, fname)
+                if is_valid_file(path):
+                    item = (path, class_to_idx[target])
+                    images.append(item)
+
+    return images
+
+
+class ClassificationDatasetFolder(Dataset):
+    """A generic data loader where the samples are arranged in this way: ::
+        root/class_x/xxx.ext
+        root/class_x/xxy.ext
+        root/class_x/xxz.ext
+        root/class_y/123.ext
+        root/class_y/nsdf3.ext
+        root/class_y/asd932_.ext
+    Args:
+        root (string): Root directory path.
+        loader (callable): A function to load a sample given its path.
+        extensions (tuple[string]): A list of allowed extensions.
+            both extensions and is_valid_file should not be passed.
+        transform (callable, optional): A function/transform that takes in
+            a sample and returns a transformed version.
+            E.g, ``transforms.RandomCrop`` for images.
+        target_transform (callable, optional): A function/transform that takes
+            in the target and transforms it.
+        is_valid_file (callable, optional): A function that takes path of a file
+            and check if the file is a valid file (used to check of corrupt files)
+            both extensions and is_valid_file should not be passed.
+     Attributes:
+        classes (list): List of the class names.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        samples (list): List of (sample path, class_index) tuples
+        targets (list): The class_index value for each image in the dataset
+    """
+
+    def __init__(self, dataset_dir, loader, extensions=None, is_valid_file=None):
+        super().__init__()
+        classes, class_to_idx = self._find_classes(self.root)
+        samples = make_dataset(self.root, class_to_idx, extensions, is_valid_file)
+        if len(samples) == 0:
+            raise (RuntimeError("Found 0 files in subfolders of: " + self.root
+                                + "\nSupported extensions are: " + ",".join(extensions)) + ".")
+
+        self.loader = loader
+        self.extensions = extensions
+
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+
+    def _find_classes(self, dir):
+        """
+        Finds the class folders in a dataset.
+        Args:
+            dir (string): Root directory path.
+        Returns:
+            tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
+        Ensures:
+            No class is a subdirectory of another.
+        """
+        classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        return classes, class_to_idx
+
+    def get_example(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, y = self.samples[index]
+        x = self.loader(path)
+        return _make_record(x_=x, y=y)
+
+    def __len__(self):
+        return len(self.samples)
+
+
+class ImageFolder(ClassificationDatasetFolder):
+    """A generic data loader where the images are arranged in this way: ::
+        root/dog/xxx.png
+        root/dog/xxy.png
+        root/dog/xxz.png
+        root/cat/123.png
+        root/cat/nsdf3.png
+        root/cat/asd932_.png
+    Args:
+        root (string): Root directory path.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        loader (callable, optional): A function to load an image given its path.
+        is_valid_file (callable, optional): A function that takes path of an Image file
+            and check if the file is a valid file (used to check of corrupt files)
+     Attributes:
+        classes (list): List of the class names.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        imgs (list): List of (image path, class_index) tuples
+    """
+
+    def __init__(self, root, loader=_load_image, is_valid_file=None):
+        super(ImageFolder, self).__init__(root, loader,
+                                          IMG_EXTENSIONS if is_valid_file is None else None,
+                                          is_valid_file=is_valid_file)
+
+
+class ImageNet(ImageFolder):
+    """`ImageNet <http://image-net.org/>`_ 2012 Classification Dataset.
+
+    Args:
+        root (string): Root directory of the ImageNet Dataset.
+        split (string, optional): The dataset split, supports ``train``, or ``val``.
+        loader (callable, optional): A function to load an image given its path.
+
+     Attributes:
+        classes (list): List of the class name tuples.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        wnids (list): List of the WordNet IDs.
+        wnid_to_idx (dict): Dict with items (wordnet_id, class_index).
+        imgs (list): List of (image path, class_index) tuples
+        targets (list): The class_index value for each image in the dataset
+    """
+    subsets = ["train", "val"]
+    default_dir = 'tiny-images'
+
+    def __init__(self, root, subset='train', **kwargs):
+        root = self.root = os.path.expanduser(root)
+
+        wnid_to_classes = torch.load(self.meta_file)[0]
+
+        super().__init__(self.split_folder, **kwargs)
+        self.root = root
+
+        self.wnids = self.classes
+        self.wnid_to_idx = self.class_to_idx
+        self.classes = [wnid_to_classes[wnid] for wnid in self.wnids]
+        self.class_to_idx = {
+            cls: idx for idx, clss in enumerate(self.classes) for cls in clss}
+
+    @property
+    def meta_file(self):
+        return os.path.join(self.root, 'meta.bin')
+
+    @property
+    def split_folder(self):
+        return os.path.join(self.root, self.split)
 
 
 # Semantic segmentation ############################################################################
