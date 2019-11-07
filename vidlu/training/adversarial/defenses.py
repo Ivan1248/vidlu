73bd64660fbf69b48f84ee4bea03d616e14609b9
Ivan <ivan.grubisic.mail@gmail.com>
2019-11-07 12:59:50 +0100

    Reduce code duplication

--- vidlu/training/adversarial/defenses.py
+++ vidlu/training/adversarial/defenses.py
@@ -2,23 +2,19 @@ import torch
 
 
 def mean_adversarial_example(x, y, class_count, attack):
-    x_advs = []
-    for c in range(class_count):
-        x_advs.append(attack.perturb(x, torch.full_like(y, c)))
+    x_advs = [attack.perturb(x, torch.full_like(y, c)) for c in range(class_count)]
     return torch.mean(torch.stack(x_advs), 0)
 
 
+def get_all_direction_predictions(x, y, model, class_count, attack):
+    return [model(attack.perturb(x, torch.full_like(y, c))) for c in range(class_count)]
+
+
 def mean_adversarial_example_prediction(x, y, model, class_count, attack):
-    x_advs, preds = [], []
-    for c in range(class_count):
-        x_advs.append(attack.perturb(x, torch.full_like(y, c)))
-        preds.append(model(x_advs[-1]))
+    preds = get_all_direction_predictions(x, y, model, class_count, attack)
     return torch.mean(torch.stack(preds), 0)
 
 
 def max_adversarial_example_prediction(x, y, model, class_count, attack):
-    x_advs, preds = [], []
-    for c in range(class_count):
-        x_advs.append(attack.perturb(x, torch.full_like(y, c)))
-        preds.append(model(x_advs[-1]))
+    preds = get_all_direction_predictions(x, y, model, class_count, attack)
     return torch.max(torch.stack(preds), 0)
