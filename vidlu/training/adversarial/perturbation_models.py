fce6fc9961d26f280f51c7b6071d8fe7534543fb
Ivan <ivan.grubisic.mail@gmail.com>
2020-01-30 17:54:42 +0100

    Update perturbation_models

--- vidlu/training/adversarial/perturbation_models.py
+++ vidlu/training/adversarial/perturbation_models.py
@@ -7,24 +7,25 @@ import vidlu.modules as vm
 import vidlu.modules.components as vmc
 import vidlu.modules.inputwise as vmi
 import vidlu.transforms.image as vti
+import vidlu.ops.image as voi
 import vidlu.ops as vo
 
 
-class Galiebwasc(vmi.PerturbationModel, vm.Seq):
+class Alglibiwasc(vm.Seq, vmi.PerturbationModel):
     def __init__(self):
-        super().__init__(gamma=vmi.AlterLogGamma((1, 1)),
-                         lightness=vmi.Multiplicative((1, 1)),
+        super().__init__(gamma=vmi.AlterLogGamma((2, 3)),
+                         lightness=vmi.Multiplicative((2, 3)),
                          bias=vmi.Additive(()),
                          warp=vmi.Warp(),
                          soft_clamp=vm.Func(partial(vo.soft_clamp, min_=0, max_=1, eps=0.01)))
 
 
-class GaTohchabTorbiwasc(vmi.PerturbationModel, vm.Seq):
+class AlgTohchabTorbiwasc(vm.Seq, vmi.PerturbationModel):
     def __init__(self):
-        super().__init__(gamma=vmi.AlterLogGamma((1, 1)),
-                         to_hsv=vm.Func(vti.rgb_to_hsv),
-                         cw_bias=vmi.Additive((1, 1)),
-                         to_rgb=vm.Func(vti.hsv_to_rgb),
+        super().__init__(gamma=vmi.AlterLogGamma((2, 3)),
+                         to_hsv=vm.Func(voi.rgb_to_hsv),
+                         cw_bias=vmi.Additive((2, 3)),
+                         to_rgb=vm.Func(voi.hsv_to_rgb),
                          bias=vmi.Additive(()),
                          warp=vmi.Warp(),
                          soft_clamp=vm.Func(partial(vo.soft_clamp, min_=0, max_=1, eps=0.01)))
