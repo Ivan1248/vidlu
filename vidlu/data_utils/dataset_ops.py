c05c2154ca57bfeb028c1081857078503631ae99
Ivan <ivan.grubisic.mail@gmail.com>
2020-03-08 19:38:02 +0100

    Add more information to an error message

--- vidlu/data_utils/dataset_ops.py
+++ vidlu/data_utils/dataset_ops.py
@@ -12,6 +12,7 @@ def rotating_labels(ds: Dataset) -> Dataset:
         [lambda d, i=i: bool(d.y == i)
          for i in range(class_count)], progress_bar=partial(tqdm, desc='rotating_labels'))[:-1]
     if any(len(csi) != len(ds) // class_count for csi in class_subset_indices):
-        raise ValueError("The distribution of labels in the dataset should be uniform.")
+        raise ValueError(f"The distribution of labels in the dataset should be uniform, "
+                         f"not {[len(csi) for csi in class_subset_indices]}.")
     indices = [class_subset_indices[i % class_count][i // class_count] for i in trange(len(ds))]
     return ds[indices]
