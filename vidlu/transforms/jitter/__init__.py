ff1de5008ec0f692c9622fc9521c2850206f9ae3
Ivan <ivan.grubisic.mail@gmail.com>
2020-03-08 20:34:43 +0100

    Refactor transforms.jitter

--- vidlu/transforms/jitter/__init__.py
+++ vidlu/transforms/jitter/__init__.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
+import warnings
 
-from .image import RandomCrop, RandomHFlip, Pad, RandomScaleCrop, PadToShape
+import torch
+
+from vidlu.transforms.image import RandomCrop, RandomHFlip, Pad, RandomScaleCrop, PadToShape
 from vidlu.utils.func import compose
+import vidlu.transforms.image as vti
+
+from .randaugment import RandAugment as PILRandAugment
 
 
 class SegmentationJitter:
@@ -56,3 +62,16 @@ class SegRandScaleCropPadHFlip(SegmentationJitter):
                              is_segmentation=(False, True))(tuple(xy))
         x, y = RandomHFlip()(xy)
         return PadToShape(self.shape, value=x.mean((1, 2)))(x), PadToShape(self.shape, value=-1)(y)
+
+
+class RandAugment(ClassificationJitter):
+    def __init__(self, n, m):
+        self.pil_randaugment = PILRandAugment(n, m)
+
+    def apply_input(self, x):
+        with torch.no_grad():
+            warnings.warn("RandAugment is not differentiable.")
+            h = x.mul(255).to(torch.uint8)
+            h = vti.torch_to_pil(h.permute(1, 2, 0).cpu())
+            h = self.pil_randaugment(h)
+            return vti.pil_to_torch(h).to(dtype=x.dtype, device=x.device).permute(2, 0, 1) / 256
