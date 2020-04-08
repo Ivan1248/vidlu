edb172ad3682616ebbdaa1ab4e2db1bf67e0ce83
Ivan <ivan.grubisic.mail@gmail.com>
2020-04-08 20:15:43 +0200

    Add /tmp to DATASETS paths

--- scripts/dirs.py
+++ scripts/dirs.py
@@ -6,13 +6,13 @@ from vidlu.utils import path
 
 def _find(path_end):
     try:
-        return Path(path.find_in_ancestor(__file__, path_end))
+        return Path(path.find_in_ancestors(__file__, path_end))
     except FileNotFoundError:
         warnings.warn(f'Cannot find directory "{path_end}".')
         return None
 
 
-DATASETS = _find('data/datasets')
+DATASETS = [Path('/tmp/'), _find('data/datasets')]
 CACHE = _find('data/cache')
 
 SAVED_STATES = _find('data/states')
