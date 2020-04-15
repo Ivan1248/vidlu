a9267663cc0e77419edf233f6bb5fe346471f3d1
Ivan <ivan.grubisic.mail@gmail.com>
2020-04-15 16:38:36 +0200

    Remove unused procedure

--- vidlu/utils/debug.py
+++ vidlu/utils/debug.py
@@ -6,18 +6,6 @@ from datetime import datetime
 from vidlu.utils.indent_print import indent_print
 
 
-def warning_debug_exec(code: str):
-    warnings.warn("Debugging with ")
-    with indent_print():
-        print(code)
-    print('in')
-    with indent_print():
-        traceback.print_stack()
-    return exec(code)
-
-
-# trace_calls
-
 def trace_calls():
     def tracefunc(frame, event, arg, indent=(0,)):
         if event == "call":
