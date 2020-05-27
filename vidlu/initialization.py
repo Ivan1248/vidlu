58542ddbc38f31397e38b740b93fefb13e66cfe6
Ivan <ivan.grubisic.mail@gmail.com>
2020-05-27 18:35:03 +0200

    Make some initializations work for "network deconvolution"

--- vidlu/initialization.py
+++ vidlu/initialization.py
@@ -2,48 +2,55 @@ import warnings
 
 from torch import nn
 
-import vidlu.modules.components as mc
-import vidlu.modules as M
+import vidlu.modules.components as vmc
+import vidlu.modules as vm
 
 
 def kaiming_resnet(module, nonlinearity='relu', zero_init_residual=False):
-    # from torchvision/models/resnet.py
-    for m in module.modules():
-        if isinstance(m, nn.Conv2d):
+    # based on initialization from torchvision/models/resnet.py
+    for name, m in module.named_modules():
+        if isinstance(m, (nn.Conv2d, vm.FastDeconv)):
             nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity=nonlinearity)
+            if getattr(m, 'bias', None) is not None:
+                nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
             nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.Linear):
             m.reset_parameters()
             nn.init.constant_(m.bias, 0)
+        elif len(m._parameters) > 0:
+            warnings.warn(f"kaiming_resnet initialization not defined for {name}: {type(m)}.")
     if zero_init_residual:
         found = 0
         for m in module.modules():
-            if isinstance(m, mc.ResNetV1Unit):
+            if isinstance(m, vmc.ResNetV1Unit):  # does not work better for mc.ResNetV2Unit
                 found += 1
-                # not mc.ResNetV2Unit because it seems not to work better
-                last_bn = [c for c in m.fork.block.children() if isinstance(c, M.BatchNorm)][-1]
+                last_bn = [c for c in m.fork.block.children() if isinstance(c, vm.BatchNorm)][-1]
                 nn.init.constant_(last_bn.orig.weight, 1e-16)
         if not found:
             warnings.warn("Batch normalization module for residual zero-init not found.")
+
 
 def kaiming_swiftnet(module, nonlinearity='relu'):
     kaiming_resnet(module, nonlinearity=nonlinearity, zero_init_residual=False)
 
 
 def kaiming_densenet(module, nonlinearity='relu'):
-    # from torchvision/models/densenet.py
-    for m in module.modules():
-        if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode='fan_in',
-                                    nonlinearity=nonlinearity)  # added nonlinearity
+    # based on initialization from torchvision/models/densenet.py
+    for name, m in module.modules():
+        if 'Conv' in type(m).__name__ and hasattr(m, 'weight'):
+            nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity=nonlinearity)
+            if getattr(m, 'bias', None) is not None:
+                nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
             nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.Linear):
             m.reset_parameters()
             nn.init.constant_(m.bias, 0)
+        elif len(m._parameters) > 0:
+            warnings.warn(f"kaiming_densenet initialization not defined for {name}: {type(m)}.")
 
 
 def kaiming_mnistnet(module, nonlinearity='relu'):
