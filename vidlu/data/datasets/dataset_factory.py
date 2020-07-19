5ede9f19815947af10056b4382b8df22c9b8a8c7
Ivan <ivan.grubisic.mail@gmail.com>
2020-07-19 18:52:31 +0200

    Add a warning when a dataset directory is not found

--- vidlu/data/datasets/dataset_factory.py
+++ vidlu/data/datasets/dataset_factory.py
@@ -2,6 +2,7 @@ import inspect
 import os
 from pathlib import Path
 import dataclasses as dc
+import warnings
 
 from . import datasets
 from .datasets import Dataset
@@ -38,13 +39,19 @@ class DatasetFactory:
         name = name.lower()
         if name not in _ds_to_info:
             raise KeyError(f'No dataset has the name "{name}".')
-        info = _ds_to_info[name]
-        subsets = info.cls.subsets
-        path_args = [vup.find_in_directories(self.datasets_dirs, info.path)] if info.path else []
-        if len(info.cls.subsets) == 0:
+        ds_info = _ds_to_info[name]
+        subsets = ds_info.cls.subsets
+        try:
+            path_args = [vup.find_in_directories(self.datasets_dirs, ds_info.path)] \
+                if ds_info.path else []
+        except FileNotFoundError as e:
+            warnings.warn(f"{ds_info.path} directory for {ds_info.cls} not found in any of "
+                          + f"{[str(p) for p in self.datasets_dirs]}")
+            path_args = [self.datasets_dirs[0] / ds_info.path]
+        if len(ds_info.cls.subsets) == 0:
             subsets = ['all']
-            load = lambda s: info.cls(*path_args, **{**info.kwargs, **kwargs})
+            load = lambda s: ds_info.cls(*path_args, **{**ds_info.kwargs, **kwargs})
         else:
-            load = lambda s: info.cls(*path_args, s, **{**info.kwargs, **kwargs})
-        splits = getattr(info.cls, 'splits', _default_splits)
+            load = lambda s: ds_info.cls(*path_args, s, **{**ds_info.kwargs, **kwargs})
+        splits = getattr(ds_info.cls, 'splits', _default_splits)
         return PartedDataset({s: load(s) for s in subsets}, splits)
