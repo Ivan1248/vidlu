f4bdc4260b17c8df0d2bf3025252f85144156dfc
Ivan <ivan.grubisic.mail@gmail.com>
2020-09-21 20:04:14 +0200

    Improve initialization

--- vidlu/models/initialization.py
+++ vidlu/models/initialization.py
@@ -6,21 +6,31 @@ import vidlu.modules.components as vmc
 import vidlu.modules as vm
 
 
+def kaiming_standard_single(m, mode, nonlinearity):
+    if isinstance(m, (nn.Conv2d, vm.FastDeconv)) or 'Conv' in type(m).__name__ and hasattr(m, 'weight'):
+        nn.init.kaiming_normal_(m.weight, mode=mode, nonlinearity=nonlinearity)
+        if getattr(m, 'bias', None) is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.Linear):
+        m.reset_parameters()
+        nn.init.constant_(m.bias, 0)
+    elif len(m._parameters) > 0:
+        return False
+    return True
+
+
+def kaiming_standard(module, mode, nonlinearity='relu'):
+    for name, m in module.named_modules():
+        if not kaiming_standard_single(m, mode, nonlinearity):
+            warnings.warn(f"Kaiming initialization not defined for {name}: {type(m)}.")
+
+
 def kaiming_resnet(module, nonlinearity='relu', zero_init_residual=False):
     # based on initialization from torchvision/models/resnet.py
-    for name, m in module.named_modules():
-        if isinstance(m, (nn.Conv2d, vm.FastDeconv)):
-            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity=nonlinearity)
-            if getattr(m, 'bias', None) is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            nn.init.constant_(m.weight, 1)
-            nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            m.reset_parameters()
-            nn.init.constant_(m.bias, 0)
-        elif len(m._parameters) > 0:
-            warnings.warn(f"kaiming_resnet initialization not defined for {name}: {type(m)}.")
+    kaiming_standard(module, mode="fan_out", nonlinearity=nonlinearity)
     if zero_init_residual:
         found = 0
         for m in module.modules():
@@ -33,38 +43,22 @@ def kaiming_resnet(module, nonlinearity='relu', zero_init_residual=False):
 
 
 def kaiming_swiftnet(module, nonlinearity='relu'):
-    return kaiming_resnet(module, nonlinearity=nonlinearity, zero_init_residual=False)
+    kaiming_standard(module, mode="fan_out", nonlinearity=nonlinearity)
 
 
 def kaiming_densenet(module, nonlinearity='relu'):
     # based on initialization from torchvision/models/densenet.py
-    for name, m in module.named_modules():
-        if 'Conv' in type(m).__name__ and hasattr(m, 'weight'):
-            nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity=nonlinearity)
-            if getattr(m, 'bias', None) is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            nn.init.constant_(m.weight, 1)
-            nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            m.reset_parameters()
-            nn.init.constant_(m.bias, 0)
-        elif len(m._parameters) > 0:
-            warnings.warn(f"kaiming_densenet initialization not defined for {name}: {type(m)}.")
+    kaiming_standard(module, mode="fan_in", nonlinearity=nonlinearity)
 
 
 def kaiming_mnistnet(module, nonlinearity='relu'):
-    for m in module.modules():
-        if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode='fan_in',
-                                    nonlinearity=nonlinearity)
+    for name, m in module.modules():
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity=nonlinearity)
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
             nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            nn.init.kaiming_normal_(m.weight, mode='fan_in',
-                                    nonlinearity=nonlinearity)
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+        elif len(m._parameters) > 0:
+            warnings.warn(f"kaiming_mnistnet initialization not defined for {name}: {type(m)}.")
