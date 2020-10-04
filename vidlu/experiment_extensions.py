915cecc52085ea4aa63b541f45423724cdee7158
Ivan <ivan.grubisic.mail@gmail.com>
2020-10-04 16:12:02 +0200

    Add experimental experiment_extension module

---
+++ vidlu/experiment_extensions.py
@@ -0,0 +1,39 @@
+import dataclasses as dc
+import os
+
+import vidlu.experiments as ve
+import vidlu.modules as vm
+from vidlu.utils.misc import Event, RemovableHandle
+
+@dc.dataclass
+class ExperimentExtension:
+    experiment: ve.TrainingExperiment
+
+    def __post_init__(self):
+        self.__enter__()
+
+    def attach(self):
+        raise NotImplementedError()
+
+    def detach(self):
+        for h in self._handles:
+            h.remove()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.detach()
+
+    def __del__(self):
+        self.detach()
+
+
+@dc.dataclass
+class ActivationStorer(ExperimentExtension):
+    dir: os.PathLike
+
+    def attach(self):
+        # TODO: collect intermediate outputs
+        model_wiot = vm.with_intermediate_outputs(self.experiment.model)
+        raise NotImplementedError()
