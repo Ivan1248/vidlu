8522da257320e80fc0a29dc50d1253c1dd4383ec
Ivan <ivan.grubisic.mail@gmail.com>
2020-11-28 22:29:08 +0100

    Fix data factory tests

--- tests/factories/test_factories.py
+++ tests/factories/test_factories.py
@@ -6,22 +6,19 @@ from vidlu.utils import tree
 
 
 def test_get_data_single(tmpdir):
-    data = factories.get_data("WhiteNoise{trainval,test}", tmpdir)
-    assert list(data.keys()) == ["WhiteNoise"]
-    assert tuple(data["WhiteNoise"].keys()) == ('trainval', 'test')
+    data = dict(factories.get_data("WhiteNoise{trainval,test}", tmpdir))
+    assert list(data.keys()) == [("WhiteNoise", "trainval"), ("WhiteNoise", "test")]
 
 
 def test_get_datasets_single_args(tmpdir):
     example_shape = (random.randint(1, 32),) * 2 + (3,)
-    data = factories.get_data(f"WhiteNoise(example_shape={example_shape}){{train,val}}", tmpdir)
-    assert list(data.keys()) == [f"WhiteNoise(example_shape={example_shape})"]
+    data = dict(factories.get_data(f"WhiteNoise(example_shape={example_shape}){{train,val}}", tmpdir))
+    assert list(data.keys())[0][0] == (f"WhiteNoise(example_shape={example_shape})")
     train, val = dict(tree.flatten(data)).values()
     assert train[0].x.shape == val[0].x.shape == example_shape
 
 
 def test_get_datasets_multiple(tmpdir):
-    with pytest.raises(ValueError):
-        factories.get_data("WhiteNoise{trainval,test}, WhiteNoise{val}", tmpdir)
-    data = factories.get_data("WhiteNoise{trainval,test}, WhiteNoise(example_shape=(8,8,8)){val}",
-                              tmpdir)
-    assert len(data) == 2 and len(tree.flatten(data)) == 3
+    data = dict(factories.get_data("WhiteNoise{trainval,test}, WhiteNoise(example_shape=(8,8,8)){val}",
+                                   tmpdir))
+    assert len(data) == 3
