4de4213583eb29f4e161f4393ba2f8c898f06d30
Ivan <ivan.grubisic.mail@gmail.com>
2020-12-08 22:47:47 +0100

    Make classification_extend_output work with tuple outputs It now takes the first element of the tuple if it gets a tuple.

--- vidlu/configs/training.py
+++ vidlu/configs/training.py
@@ -96,10 +96,11 @@ class TrainerConfig(NameDict):
 # Extend output
 
 def classification_extend_output(output):
-    if not isinstance(output, torch.Tensor):
-        raise ValueError("The output must ba a `torch.Tensor`.")
-    logits = output
-    return logits, Record(output=logits, log_probs_=lambda: logits.log_softmax(1),
+    logits = output[0] if isinstance(output, tuple) else output
+    if not isinstance(logits, torch.Tensor):
+        raise ValueError("The output or its first element must be a `torch.Tensor`"
+                         + " representing class scores.")
+    return logits, Record(output=logits, full_output=output, log_probs_=lambda: logits.log_softmax(1),
                           probs_=lambda r: r.log_probs.exp(),
                           hard_prediction_=lambda: logits.argmax(1))
 
