1558a981dd2e5d6366bb690a05255b11b7acee7d
Ivan <ivan.grubisic.mail@gmail.com>
2020-12-09 11:40:32 +0100

    Update module hooks

--- vidlu/modules/utils/hooks.py
+++ vidlu/modules/utils/hooks.py
@@ -10,10 +10,22 @@ def check_no_inf_nan(module, input, output):
         >>> for m in module.modules:
         >>>      m.register_forward_hook(check_no_inf_nan)
     """
-    if (inf := torch.isinf(output).any()) or (nan := torch.isnan(output).any()):
-        module_name = try_get_module_name_from_call_stack(module)
-        print(f"{module_name=}, {inf=}, {nan=}")
-        breakpoint()
+    for output_index, out in enumerate(extract_tensors(output)):
+        if (inf := torch.isinf(out).any()) or (nan := torch.isnan(out).any()):
+            module_name = try_get_module_name_from_call_stack(module)
+            print(f"{module_name=}, {output_index=} {inf=}, {nan=}")
+            breakpoint()
+
+
+def print_norm(module, input, output):
+    """
+    Example:
+        >>> for m in module.modules:
+        >>>      m.register_forward_hook(check_no_inf_nan)
+    """
+    module_name = try_get_module_name_from_call_stack(module)
+    for output_index, out in enumerate(extract_tensors(input)):
+        print(f"{module_name=}, [{output_index}] l2_norm={out.norm().item()}, linf_norm={out.abs().max()}")
 
 
 def check_propagates_log_abs_det_jac(module, input, output):
