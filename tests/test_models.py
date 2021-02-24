3b262e6224adfa3b1da55afb1f02b133eea9a0f0
Ivan <ivan.grubisic.mail@gmail.com>
2021-02-24 19:36:18 +0100

    Add more is_built and inverse tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -20,6 +20,7 @@ def compare_with_torchvision(mymodelname, tvmodelname):
     x = torch.randn(2, 3, 224, 224)  # TODO: investigate: 64x64 doesn't work on linux
     vidlu_model = factories.get_model(
         mymodelname, problem=problem.Classification(class_count=8), init_input=x, verbosity=2)
+    assert vm.is_built(vidlu_model, thorough=True)
     tv_model = torchvision.models.__dict__[tvmodelname](num_classes=8)
 
     vidlu_model.eval()
@@ -64,12 +65,23 @@ class TestIRevNet:
             "IRevNet,backbone_f=t(init_stride=4,base_width=None,group_lengths=(2,1))",
             problem=problem.Classification(class_count=8),
             init_input=x)
+        assert vm.is_built(model, thorough=True)
         model_injective = vm.deep_split(model, 'backbone.concat')[0]
+        assert vm.is_built(model_injective, thorough=True)
+
         h = model_injective(x)
-        model_injective.inverse(h)
+        inverse_built = vm.is_built(model_injective.inverse)
+        assert not inverse_built
+        assert vm.is_built(model_injective.inverse, thorough=True) == inverse_built
+
+        for keep_inv_ref in [True, False]:
+            inv_ref = model_injective.inverse if keep_inv_ref else None
+            model_injective.inverse(h)
+            assert (model_injective.inverse is inv_ref) == keep_inv_ref
+            assert vm.is_built(model_injective.inverse) == keep_inv_ref
+
         # numerical error too high for randomly initialized model and high-frequecy input
         # model_injective.check_inverse(x)
-
         for should_error in [False, True]:
             for m in (model if should_error else model_injective).modules():
                 m.register_forward_hook(vmu.hooks.check_propagates_log_abs_det_jac)
