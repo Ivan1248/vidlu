8769a3e29b9cad45107ef9f8a5ed49949773d94c
Ivan <ivan.grubisic.mail@gmail.com>
2021-03-11 16:01:58 +0100

    Improve a comment

--- vidlu/data/utils/dataset_ops.py
+++ vidlu/data/utils/dataset_ops.py
@@ -19,6 +19,6 @@ def rotating_labels(ds: Dataset) -> Dataset:
     if any(len(csi) != len(ds) // class_count for csi in class_subset_indices):
         raise ValueError(f"The distribution of labels in the dataset should be uniform, "
                          f"not {[len(csi) for csi in class_subset_indices]}.")
-    # rearrange examples so that class = i mod class_count
+    # rearrange examples so that ds[i].y = i mod class_count
     indices = [class_subset_indices[i % class_count][i // class_count] for i in trange(len(ds))]
     return ds[indices]
