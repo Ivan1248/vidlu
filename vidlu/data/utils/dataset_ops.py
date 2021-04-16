dfa834422162881b70c905a3d4a9cf7e733be82d
Ivan <ivan.grubisic.mail@gmail.com>
2021-04-16 18:19:46 +0200

    Add folds and chunks to dataset_ops

--- vidlu/data/utils/dataset_ops.py
+++ vidlu/data/utils/dataset_ops.py
@@ -1,8 +1,10 @@
-from vidlu.utils.func import partial
+from warnings import warn
 
+import numpy as np
 from tqdm import tqdm, trange
 
-from vidlu.data import Dataset
+from vidlu.utils.func import partial
+from vidlu.data import Dataset, Record
 
 
 def rotating_labels(ds: Dataset) -> Dataset:
@@ -22,3 +24,23 @@ def rotating_labels(ds: Dataset) -> Dataset:
     # rearrange examples so that ds[i].y = i mod class_count
     indices = [class_subset_indices[i % class_count][i // class_count] for i in trange(len(ds))]
     return ds[indices]
+
+
+def folds(ds: Dataset, n: int):
+    indices = np.linspace(0.5, len(ds) + 0.5, n + 1, dtype=int)
+
+    def get_fold_f(i):
+        return lambda: ds[indices[i]:indices[i + 1]]
+
+    return Record({f"{i}_": get_fold_f(i) for i in range(n)})
+
+
+def chunks(ds: Dataset, size: int):
+    if len(ds) % size != 0:
+        warn(f"Dataset size ({len(ds)}) is not divisible by chunk size ({size}).")
+    indices = [x * size for x in range(len(ds) // size)]
+
+    def get_fold_f(i):
+        return lambda: ds[indices[i]:indices[i + 1]]
+
+    return Record({f"{i}_": get_fold_f(i) for i in range(len(indices) - 1)})
