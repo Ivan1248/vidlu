c916750df448733463998d1d1efe475aea3406f5
Ivan <ivan.grubisic.mail@gmail.com>
2021-06-21 22:48:52 +0200

    Fix a typo

--- scripts/papers/semisup_efficient_mva21/generate_images.sh
+++ scripts/papers/semisup_efficient_mva21/generate_images.sh
@@ -2,7 +2,7 @@ set -e
 
 echo "Reproducing figure with outputs of models trained on half-resolution Cityscapes train with 1/4 of the labels"
 
-echo "Generaiing inputs..."
+echo "Generating inputs..."
 CUDA_VISIBLE_DEVICES=0 python run.py test "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(53),4)[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_phtps20_seg,train_step=ts.MeanTeacherStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=800,batch_size=[8,8]" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" -r --module "papers.semisup_efficient_mva21.generate_images:generate_inputs,e,n=8,dir='/tmp/semisup'"
 
 echo "Generaiing results (semi-supervised MT-PhTPS)..."
