0d3693947179b3a0f607a1659647e3602a82c4e4
Ivan <ivan.grubisic.mail@gmail.com>
2021-07-08 19:42:16 +0200

    Update MVA21 scripts

--- scripts/papers/semisup_efficient_mva21/experiments_label_proportions.sh
+++ scripts/papers/semisup_efficient_mva21/experiments_label_proportions.sh
@@ -15,7 +15,7 @@ for divisor in 8 4 2 1; do  # How many times the labeled dataset is smaller than
   # An epoch corresponds to one pass through labeled data. When not all labeled examples are used, more epochs are needed to get the same number of iterations.
   let epoch_count=divisor*100  # 800, 400, 200, 100 (always 74400 iterations)
   printf "\n${name}: 1/$divisor labeled examples, $epoch_count epochs\n\n"
-  python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=4" --params "id->backbone:deeplabv1_resnet101-coco.pth" --no_init_eval --resume_or_start
+  python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=4" --params "id:->backbone:deeplabv1_resnet101-coco" --no_init_eval --resume_or_start
 done
 
 name="DL MT-PhTPS"
@@ -25,7 +25,7 @@ let seed=start_seed
 for divisor in 8 4 2 1; do
   let epoch_count=divisor*100  # 800, 400, 200, 100
   printf "\n${name}: 1/$divisor labeled examples, $epoch_count epochs\n\n"
-  python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,tc.semisup_cons_phtps20_seg,train_step=ts.MeanTeacherStep(alpha=0.5,mem_efficient=True),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[4,4]" --params "id->backbone:deeplabv1_resnet101-coco.pth" --no_init_eval --resume_or_start
+  python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,tc.semisup_cons_phtps20_seg,train_step=ts.MeanTeacherStep(alpha=0.5,mem_efficient=True),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[4,4]" --params "id:->backbone:deeplabv1_resnet101-coco" --no_init_eval --resume_or_start
 done
 
 name="DL MT-CutMix with L2 loss, confidence thresholding"
@@ -35,7 +35,7 @@ let seed=start_seed
 for divisor in 8 4 2 1; do
   let epoch_count=divisor*100  # 800, 400, 200, 100
   printf "\n${name}: 1/$divisor labeled examples, $epoch_count epochs\n\n"
-  python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.MeanTeacherStep(loss_cons=partial(losses.conf_thresh_probs_sqr_l2_dist_ll,conf_thresh=0.97)),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[4,4]" --params "id->backbone:deeplabv1_resnet101-coco.pth" --no_init_eval --resume_or_start
+  python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.MeanTeacherStep(loss_cons=partial(losses.conf_thresh_probs_sqr_l2_dist_ll,conf_thresh=0.97)),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[4,4]" --params "id:->backbone:deeplabv1_resnet101-coco" --no_init_eval --resume_or_start
 done
 
 # SwiftNet-RN18
@@ -49,7 +49,7 @@ for s in {0..0}; do
   do
     let epoch_count=divisor*200  # 1600, 800, 400, 200 (always 74400 iterations)
     printf "\n${name}: seed=$seed, 1/$divisor labeled examples, $epoch_count epochs\n\n"
-    python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=8" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" --resume_or_start
+    python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=8" --params "resnet:backbone->backbone.backbone:resnet18" --resume_or_start
   done
 done
 
@@ -62,7 +62,7 @@ for s in {0..4}; do
   do
     let epoch_count=divisor*200  # 1600, 800, 400, 200
     printf "\n${name}: seed=$seed, 1/$divisor labeled examples, $epoch_count epochs\n\n"
-    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_phtps20_seg,train_step=ts.SemisupVATTrainStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" --resume_or_start
+    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_phtps20_seg,train_step=ts.SemisupVATTrainStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet:backbone->backbone.backbone:resnet18" --resume_or_start
   done
 done
 
@@ -75,7 +75,7 @@ for s in {0..4}; do
   do
     let epoch_count=divisor*200  # 1600, 800, 400, 200
     printf "\n${name}: seed=$seed, 1/$divisor labeled examples, $epoch_count epochs\n\n"
-    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.SemisupVATTrainStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" --resume_or_start
+    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.SemisupVATTrainStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet:backbone->backbone.backbone:resnet18" --resume_or_start
   done
 done
 
@@ -88,7 +88,7 @@ for s in {0..4}; do
   do
     let epoch_count=divisor*200  # 1600, 800, 400, 200
     printf "\n${name}: seed=$seed, 1/$divisor labeled examples, $epoch_count epochs\n\n"
-    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_phtps20_seg,train_step=ts.MeanTeacherStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" --resume_or_start
+    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_phtps20_seg,train_step=ts.MeanTeacherStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet:backbone->backbone.backbone:resnet18" --resume_or_start
   done
 done
 
@@ -101,7 +101,7 @@ for s in {0..4}; do
   do
     let epoch_count=divisor*200  # 1600, 800, 400, 200
     printf "\n${name}: seed=$seed, 1/$divisor labeled examples, $epoch_count epochs\n\n"
-    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.MeanTeacherStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" --resume_or_start
+    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.MeanTeacherStep(alpha=0.5),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet:backbone->backbone.backbone:resnet18" --resume_or_start
   done
 done
 
@@ -114,6 +114,6 @@ for s in {0..4}; do
   do
     let epoch_count=divisor*200  # 1600, 800, 400, 200
     printf "\n${name}: seed=$seed, 1/$divisor labeled examples, $epoch_count epochs\n\n"
-    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.MeanTeacherStep(loss_cons=partial(losses.conf_thresh_probs_sqr_l2_dist_ll,conf_thresh=0.97)),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" --resume_or_start
+    python run.py train "train,train_u,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(${seed}),${divisor})[0],d[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,tc.semisup_cons_cutmix,train_step=ts.MeanTeacherStep(loss_cons=partial(losses.conf_thresh_probs_sqr_l2_dist_ll,conf_thresh=0.97)),lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=[8,8]" --params "resnet:backbone->backbone.backbone:resnet18" --resume_or_start
   done
 done
--- scripts/papers/semisup_efficient_mva21/test_setup.sh
+++ scripts/papers/semisup_efficient_mva21/test_setup.sh
@@ -7,11 +7,11 @@ epoch_count=1  # set to 0 for evaluation only
 
 # SwiftNet-RN18, half-resolution Cityscapes
 printf "\n\nSwiftNet-RN18, half-resolution Cityscapes\n"
-python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(53),4)[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=8" --params "resnet[backbone]->backbone.backbone:resnet18-5c106cde.pth" --no_train_eval
+python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(53),4)[0],d[1])" "standardize(cityscapes_mo)" "SwiftNet,backbone_f=t(depth=18,small_input=False)" "tc.swiftnet_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=8" --params "resnet:backbone->backbone.backbone:resnet18" --no_train_eval
 
 # DeepLab-v2-RN101, half-resolution Cityscapes
 printf "\n\nDeepLab-v2-RN101, half-resolution Cityscapes\n"
-python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(53),4)[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=4" --params "id->backbone:deeplabv1_resnet101-coco.pth" --no_init_eval --no_train_eval
+python run.py train "train,test:Cityscapes(downsampling=2){train,val}:(folds(d[0].permute(53),4)[0],d[1])" "standardize(cityscapes_mo)" "SegmentationModel,init=None,backbone_f=partial(vmo.deeplab.DeepLabV2_ResNet101,n_classes=19),head_f=vmc.ResizingHead" "tc.deeplabv2_cityscapes_halfres,lr_scheduler_f=lr.QuarterCosLR,epoch_count=${epoch_count},batch_size=4" --params "id:->backbone:deeplabv1_resnet101-coco" --no_init_eval --no_train_eval
 
 # WRN-28-2, CIFAR-10
 printf "\n\nWRN-28-2, CIFAR-10\n"
