15044fd9f56fc1f1c306910a5f75a39cec1905c8
Ivan <ivan.grubisic.mail@gmail.com>
2021-09-14 12:54:22 +0200

    Add Imagenet pixel stats

--- vidlu/configs/data/stats.py
+++ vidlu/configs/data/stats.py
@@ -3,3 +3,5 @@ imagenet_mo = dict(mean=[73.15 / 255, 82.9 / 255, 72.3 / 255],
 cityscapes_mo = imagenet_mo
 cityscapes = dict(mean=[0.28689554, 0.32513303, 0.28389177],
                   std=[0.18696375, 0.19017339, 0.18720214])
+imagenet = dict(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])
