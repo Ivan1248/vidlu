ad0239dcdb6817d5ddfffe2d73b7c1797d51d1cd
Ivan <ivan.grubisic.mail@gmail.com>
2021-10-20 15:59:29 +0200

    Remove scripts/papers

--- scripts/papers/semisup_efficient_mva21/_context.py
+++
@@ -1,5 +0,0 @@
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str((Path(__file__).parent / '../../..').resolve()))  # vidlu
-sys.path.insert(0, str((Path(__file__).parent / '../..').resolve()))  # dirs
