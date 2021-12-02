a5fc7f18e29d9654248e99412108261c22f2e223
Ivan <ivan.grubisic.mail@gmail.com>
2021-12-02 19:18:39 +0100

    Add tests for more NameDict constraints

--- tests/utils/test_collections.py
+++ tests/utils/test_collections.py
@@ -8,8 +8,10 @@ import vidlu.utils.collections as vuc
 class TestNameDict:
     def test_namedict(self):
         nd = vuc.NameDict(dict(a=5), b=6, c=7)
-        assert len(nd) == 3
         assert nd.a == nd['a']
+        assert len(nd) == 3
+        assert vars(nd) is nd.__dict__
+        assert vuc.NameDict(vars(nd)) == nd
         with pytest.raises(AttributeError):
             nd.d
         with pytest.raises(KeyError):
