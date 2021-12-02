3249a4cdcf714d9e5d9dff8575d2827e89ed554f
Ivan <ivan.grubisic.mail@gmail.com>
2021-12-02 19:54:39 +0100

    Improve data.utils.caching, and an add_segmentation_class_info_lazily procedure

--- vidlu/data/utils/caching.py
+++ vidlu/data/utils/caching.py
@@ -10,6 +10,9 @@ from tqdm import tqdm
 from vidlu.data import DatasetFactory
 from vidlu.data.record import Record
 from vidlu.utils import path
+from vidlu.utils.collections import NameDict
+from vidlu.data.utils.class_incidence import seg_class_info
+from vidlu.data.data_loader import SingleDataLoader
 
 
 # General
@@ -39,8 +42,8 @@ def pds_add_info_lazily(parted_dataset, cache_dir, name, source_ds=None):
 # Standardization ##################################################################################
 
 def compute_pixel_stats(dataset, div255=False, progress_bar=False):
-    pbar = tqdm if progress_bar else lambda x: x
-    images = (np.array(r[0]) for r in pbar(dataset))
+    pbar = partial(tqdm, desc='compute_pixel_stats') if progress_bar else lambda x: x
+    images = (np.array(r) for r in pbar(SingleDataLoader(dataset, num_workers=4)))
     mvn = np.array([(x.mean((0, 1)), x.var((0, 1)), np.prod(x.shape[:2])) for x in images])
     means, vars_, ns = [mvn[:, i] for i in range(3)]  # means, variances, pixel counts
     ws = ns / ns.sum()  # image weights (pixels in image / pixels in all images)
@@ -59,45 +62,59 @@ def _compute_pixel_stats_d(ds):
     return Namespace(mean=mean, std=std)
 
 
-def add_pixel_stats_to_info_lazily(parted_dataset, cache_dir):
+def _get_training_dataset(parted_dataset):
     pds = parted_dataset
-
     try:
         stats_ds = pds.trainval if 'trainval' in pds.keys() else pds.train.join(pds.val)
     except KeyError:
         part_name, stats_ds = next(iter(pds.items()))
         warnings.warn('The parted dataset object has no "trainval" or "train" and "val" parts.'
                       + f' "{part_name}" is used instead.')
+    return stats_ds
 
-    ds_with_info = stats_ds.info_cache_hdd(dict(pixel_stats=_compute_pixel_stats_d), cache_dir)
+
+def add_pixel_stats_to_info_lazily(parted_dataset, cache_dir):
+    stats_ds = _get_training_dataset(parted_dataset)
+    ds_with_info = stats_ds.info_cache_hdd(NameDict(pixel_stats=_compute_pixel_stats_d), cache_dir)
 
     def cache_transform(ds):
         return ds.info_cache(
-            dict(pixel_stats=partial(_get_info, source_ds=ds_with_info, name='pixel_stats')))
+            NameDict(pixel_stats=partial(_get_info, source_ds=ds_with_info, name='pixel_stats')))
 
-    return _map_record(cache_transform, pds)
+    return _map_record(cache_transform, parted_dataset)
+
+
+def add_segmentation_class_info_lazily(parted_dataset, cache_dir):
+    def cache_transform(ds):
+        if 'seg_map' not in ds[0].keys():
+            return ds
+        ds = ds.info_cache_hdd(NameDict(seg_class_info=seg_class_info), cache_dir, recompute=False)
+        return ds.enumerate().map_unpack(
+            lambda i, r: Record(r,
+                                class_seg_boxes=ds.info.seg_class_info['class_segment_boxes'][i]))
+
+    return _map_record(cache_transform, parted_dataset)
 
 
 # Caching ##########################################################################################
 
 def cache_data_lazily(parted_dataset, cache_dir, min_free_space=20 * 2 ** 30):
     def transform(ds):
+        ds_cached = ds.cache_hdd(f"{cache_dir}/datasets")
+
         elem_size = ds.example_size(sample_count=4)
         size = len(ds) * elem_size
         free_space = shutil.disk_usage(cache_dir).free
-
-        ds_cached = ds.cache_hdd(f"{cache_dir}/datasets")
         cached_size = path.get_size(ds_cached.cache_dir)
         if cached_size > size * 0.1 or free_space + cached_size - size >= min_free_space:
-            ds = ds_cached
+            return ds_cached
         else:
             warnings.warn(f'The dataset {ds.identifier} will not be cached because there is not'
                           + f' much space left.'
                           + f' Available space: {(free_space + cached_size) / 2 ** 30:.3f} GiB.'
                           + f' Data size: {size / 2 ** 30:.3f} GiB.')
             ds_cached.delete_cache()
-            del ds_cached
-        return ds
+            return ds
 
     return _map_record(transform, parted_dataset)
 
