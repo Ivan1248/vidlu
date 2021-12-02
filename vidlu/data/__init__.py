5576c1fe11b75d99dc2bf259f01f611bbb46685e
Ivan <ivan.grubisic.mail@gmail.com>
2021-12-02 19:49:29 +0100

    Update data.__init__

--- vidlu/data/__init__.py
+++ vidlu/data/__init__.py
@@ -1,5 +1,6 @@
-from .misc import pickle_sizeof, default_collate
 from .record import Record
 from .dataset import Dataset, clear_hdd_cache, clean_up_dataset_cache
 from .datasets import DatasetFactory
-from .data_loader import DataLoader, ZipDataLoader, BatchTuple
+from .data_loader import DataLoader, BatchTuple, ZipDataLoader, CombinedDataLoader
+from .collation import default_collate
+from . import types
