0f60ef9e8a9b83586719103891a6009968c0ed09
Ivan <ivan.grubisic.mail@gmail.com>
2022-03-03 18:13:36 +0100

    Add DeepLabV2RN101 and DeepLabV3Plus models

--- vidlu/models/other.py
+++ vidlu/models/other.py
@@ -1,1 +1,27 @@
-from vidlu.modules.other.deeplab import DeepLabV2_ResNet101
+from functools import partial
+import typing as T
+
+import torch
+
+import vidlu.modules.components as vmc
+from .models import SegmentationModel
+
+
+class DeepLabV2RN101(SegmentationModel):
+    def __init__(self, num_classes):
+        super().__init__(
+            backbone_f=partial(torch.hub.load, 'Ivan1248/cutmix-semisup-seg',
+                               'resnet101_deeplab_imagenet', num_classes=num_classes),
+            init=None,
+            head_f=vmc.ResizingHead)
+
+
+class DeepLabV3Plus(SegmentationModel):
+    def __init__(self, backbone: T.Literal['resnet50', 'resnet101', 'xception', 'drn', 'mobilenet'],
+                 num_classes: int, pretrained=False):
+        super().__init__(
+            backbone_f=partial(torch.hub.load, 'Ivan1248/Context-Aware-Consistency',
+                               'DeepLabV3Plus', backbone=backbone, num_classes=num_classes,
+                               pretrained=pretrained),
+            init=None,
+            head_f=partial(vmc.SegmentationHead, class_count=num_classes))
