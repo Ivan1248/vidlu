f40c79bacf89431180d6dd165506132d80b94fbe
Ivan <ivan.grubisic.mail@gmail.com>
2022-04-01 12:59:20 +0200

    Update the pert_model.py example

--- scripts/mini_experiments/pert_model.py
+++ scripts/mini_experiments/pert_model.py
@@ -17,14 +17,19 @@ def sample_pmodel(*args):
 
 ds = HBlobs().map(lambda r: type(r)(image=tvtf.to_tensor(r.image)))
 
-img = ds[0].image.unsqueeze(0)
-img = Image(img)  # ovako će vjerojatno trebati u budućnosti radi fleksibilnosti
+img = ds[0].image.unsqueeze(0)  # NCHW
+mask = torch.ones((img.shape[0], *img.shape[2:]))
+segmap = torch.ones(img.shape)
 
 pmodel = sample_pmodel(img)
 
-img_p = pmodel(img)
+img_p, segmap_p, mask_p = pmodel(img, segmap, mask)
 
-img_grid = make_grid(torch.cat([img, img_p], dim=0))
+mask_to_image = lambda m: m.unsqueeze(1).expand(m.shape[0], 3, *m.shape[1:])
+
+img_grid = make_grid(torch.cat(
+    [img, segmap, mask_to_image(mask), img_p, segmap_p, mask_to_image(mask_p)],
+    dim=0), nrow=3)
 
 plt.imshow(img_grid.permute(1, 2, 0).cpu().numpy())
 plt.show()
