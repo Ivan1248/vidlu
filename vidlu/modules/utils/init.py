d922a30ffcb7a32ab251f85be0ef16a58830c2c1
Ivan Grubišić <ivan.grubisic.mail@gmail.com>
2023-01-22 16:59:20 +0100

    Add BinaryInit and JointInit

--- vidlu/modules/utils/init.py
+++ vidlu/modules/utils/init.py
@@ -36,12 +36,47 @@ class UniformInit(Initializer):
 
 
 @dc.dataclass
+class BinaryInit(Initializer):
+    name_to_prob: T.Mapping[str, float]
+
+    def __call__(self, module, input=None):
+        for path, prob in self.name_to_prob.items():
+            param = vm.get_submodule(module, path)
+            param.set_(torch.rand(()) <= prob)
+
+
+@dc.dataclass
 class NormalInit(Initializer):
     name_to_mean_std: T.Mapping[str, T.Tuple[float, float]]
 
     def __call__(self, module, input=None):
         for path, (mean, std) in self.name_to_mean_std.items():
             vm.get_submodule(module, path).normal_(mean=mean, std=std)
+
+
+class JointInit(Initializer):
+    def __init__(self, *args):
+        self.initializers = args
+
+    def __call__(self, module, input=None):
+        for init in self.initializers:
+            init(module, input=input)
+
+    def __getitem__(self, item):
+        return self.initializers[item]
+
+    def register_as_pre_forward_hook(self, module, repeat=False):
+        def hook(module, input):
+            if not repeat:
+                handle.remove()
+            for init in self.initializers:
+                init.register_as_pre_forward_hook(module)
+
+        handle = module.register_forward_pre_hook(hook)
+        return handle
+
+    def __repr__(self):
+        return f"{type(self).__name__}({', '.join(map(str, self.initializers))})"
 
 
 class MultiInit(Initializer):
@@ -65,6 +100,9 @@ class MultiInit(Initializer):
 
     def __getattr__(self, item):
         return self[item]
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.initializers})"
 
     def register_as_pre_forward_hook(self, module, repeat=False):
         def hook(module, input):
