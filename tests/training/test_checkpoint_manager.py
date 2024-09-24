8c6e8ff5a90ae051990d9ffb3b2cc35482837e45
Ivan Grubišić <ivan.grubisic.mail@gmail.com>
2024-09-24 14:47:45 +0200

    Update CheckpointManager tests

--- tests/training/test_checkpoint_manager.py
+++ tests/training/test_checkpoint_manager.py
@@ -8,7 +8,7 @@ from vidlu.training.checkpoint_manager import CheckpointManager
 @pytest.mark.parametrize("scores,kept", [({1: 2, 2: 4, 3: 3, 4: 2, 5: 1, 6: 0}, {2, 3, 5, 6})])
 def test_training_experiment(tmpdir, scores, kept):
     cpman_f = partial(CheckpointManager, checkpoints_dir=tmpdir, experiment_name="test",
-                      info=dict(name="Foo"), n_last_kept=2, n_best_kept=2,
+                      experiment_info=dict(name="Foo"), n_recent_kept=2, n_best_kept=2,
                       perf_func=lambda s: s['s'])
     cpman = cpman_f()
     for i, s in scores.items():
@@ -17,7 +17,7 @@ def test_training_experiment(tmpdir, scores, kept):
     for mode in ['resume', 'resume_or_start']:
         with pytest.raises(RuntimeError):
             cpman2 = cpman_f()
-        cpman2: CheckpointManager = cpman_f(mode=mode)
+        cpman2: CheckpointManager = cpman_f(start_mode=mode)
         assert set(cpman2.saved) == set(map(str, kept))
 
     with pytest.raises(RuntimeError):
@@ -28,10 +28,10 @@ def test_training_experiment(tmpdir, scores, kept):
     cpman2.remove_old_checkpoints(0, 0)
     for mode in ['start', 'resume_or_start']:
         with pytest.raises(RuntimeError):
-            cpman_f(mode='resume')
-        cpman3: CheckpointManager = cpman_f(mode=mode)
+            cpman_f(start_mode='resume')
+        cpman3: CheckpointManager = cpman_f(start_mode=mode)
         assert len(cpman3.saved) == 0
 
     cpman3.save(dict(s=8), dict(i=0, s=8))
-    cpman4: CheckpointManager = cpman_f(mode='restart')
+    cpman4: CheckpointManager = cpman_f(start_mode='restart')
     assert len(cpman4.saved) == 0
