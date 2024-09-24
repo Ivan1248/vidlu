58c17f0f7a0ba2baff9023305196ae5712aee5a7
Ivan Grubišić <ivan.grubisic.mail@gmail.com>
2024-09-24 11:40:35 +0200

    Update jitter.py

--- vidlu/transforms/jitter/jitter.py
+++ vidlu/transforms/jitter/jitter.py
@@ -5,11 +5,13 @@ import typing as T
 from functools import partial
 
 import torch
+import torchvision.transforms as tvt
 
 from vidlu.utils.func import compose
 import vidlu.transforms.image as vti
 import vidlu.modules as vm
 import vidlu.modules.pert as pert
+import vidlu.modules.utils.init as vmui
 import vidlu.data.types as dt
 
 from .randaugment import RandAugment as PILRandAugment
@@ -23,35 +25,6 @@ class Composition:
         for f in self.jitters:
             x = f(x)
         return x
-
-
-class PhTPS20:
-    def __init__(self):
-        from vidlu.training.robustness import perturbation as pert
-        from ...modules.utils import init
-        self.pert_model = pert.PhotoTPS20()
-        self.init = init.MultiInit(
-            dict(tps=init.NormalInit({'offsets': (0, 0.1)}),
-                 photometric=init.UniformInit(
-                     {'add_v.addend': [-0.25, 0.25],
-                      'mul_s.factor': [0.25, 2.],
-                      'add_h.addend': [-0.1, 0.1],
-                      'mul_v.factor': [0.25, 2.]})))
-
-    def __call__(self, inputs):
-        (x, y), put_back = pick(lambda a: isinstance(a, (dt.ArraySpatial2D, dt.ClassLabel)), inputs)
-        x_ = x.unsqueeze(0)
-        with torch.no_grad():
-            if not vm.is_built(self.pert_model):
-                self.pert_model(x_)
-                for p in self.pert_model.parameters():
-                    p.requires_grad = False
-            self.init(self.pert_model, x_)
-        if isinstance(y, dt.ClassLabel):
-            x = self.pert_model(x_).squeeze(0)
-        else:
-            x, y = self.pert_model((x, y))
-        return put_back((x, y))
 
 
 class SegmentationJitter(pert.PertModel):
@@ -89,7 +62,7 @@ def _args_repr(obj):
 
 
 def unsupported_error(tranform, x):
-    raise NotImplementedError(f'Domain {type(x)} not supported by {tranform}.')
+    raise NotImplementedError(f'Modality {type(x)} not supported by {tranform}.')
 
 
 class CifarPadRandCropHFlip(ClassificationJitter):
@@ -110,14 +83,29 @@ class SegRandCropHFlip(SegmentationJitter):
         return compose(vti.RandomCrop(self.crop_shape), vti.RandomHFlip())(tuple(x))
 
 
-def pick(cond, inputs):
-    # TODO: replace filt with indices?
-    filt = {ind: i for i, ind in enumerate([i for i, x in enumerate(inputs) if cond(x)])}
+def pick_out(cond: T.Callable, inputs: tuple) -> T.Tuple[tuple, T.Callable[[tuple], tuple]]:
+    # Dictionary to track indices of filtered elements
+    filtered_indices = {ind: i for i, ind in enumerate(i for i, x in enumerate(inputs) if cond(x))}
 
-    def put_back(outputs):
-        return tuple(outputs[filt[i]] if i in filt else x for i, x in enumerate(inputs))
+    def put_back_together(outputs: tuple) -> tuple:
+        # Reinsert filtered elements into their original positions
+        return tuple(outputs[filtered_indices[i]] if i in filtered_indices else x
+                     for i, x in enumerate(inputs))
 
-    return tuple(inputs[i] for i in filt), put_back
+    # Tuple of filtered elements and the function for putting the tuple back together
+    return tuple(inputs[i] for i in filtered_indices), put_back_together
+
+
+def only_applies_to(type):
+    def decorator(func):
+        def wrapper(inputs):
+            d, put_back = pick_out(lambda x: isinstance(x, type), inputs)
+            d = func(d)
+            return put_back(d)
+
+        return wrapper
+
+    return decorator
 
 
 class SegRandScaleCropPadHFlip(pert.PertModel):
@@ -131,7 +119,7 @@ class SegRandScaleCropPadHFlip(pert.PertModel):
                  scale_factor_fn=lambda h, w: 1.,
                  overflow: object = 0,
                  align_corners: bool = True,
-                 image_pad_value: T.Union[torch.Tensor, float, T.Literal['mean']] = 'mean',
+                 image_pad_value: T.Union[torch.Tensor, float, T.Literal['mean']] = 'mean',  # 0.?
                  label_pad_value=-1,
                  random_scale_crop_f=vti.RandomScaleCrop,
                  scale_dist: vti.ScaleDistArg = "uniform"):
@@ -140,19 +128,22 @@ class SegRandScaleCropPadHFlip(pert.PertModel):
         self.__dict__.update(self.get_args(locals()))
 
     def forward(self, inputs):
-        d, put_back = pick(lambda x: isinstance(x, dt.Spatial2D), inputs)
-        d = self.random_scale_crop_f(
-            shape=self.shape, max_scale=self.max_scale, min_scale=self.min_scale,
-            overflow=self.overflow, scale_factor_fn=self.scale_factor_fn,
-            align_corners=self.align_corners, scale_dist=self.scale_dist)(d)
-        d = vti.RandomHFlip()(d)
-        pad = partial(vti.pad_to_shape, shape=self.shape)
-        d = [pad(x, value=self.image_pad_value) if isinstance(x, dt.Image) else
-             pad(x, value=self.label_pad_value) if isinstance(x, dt.SegMap) else
-             pad(x, value=0) if isinstance(x, dt.Spatial2D) else
-             x
-             for x in d]
-        return put_back(d)
+        @only_applies_to(dt.Spatial2D)
+        def inner_forward(d: T.Sequence[dt.Spatial2D]):
+            d = self.random_scale_crop_f(
+                shape=self.shape, max_scale=self.max_scale, min_scale=self.min_scale,
+                overflow=self.overflow, scale_factor_fn=self.scale_factor_fn,
+                align_corners=self.align_corners, scale_dist=self.scale_dist)(d)
+            d = vti.RandomHFlip()(d)
+            pad = partial(vti.pad_to_shape, shape=self.shape)
+            d = [pad(x, value=self.image_pad_value) if isinstance(x, dt.Image) else
+                 pad(x, value=self.label_pad_value) if isinstance(x, dt.SegMap) else
+                 pad(x, value=0) if isinstance(x, dt.Spatial2D) else
+                 x
+                 for x in d]
+            return d
+
+        return inner_forward(inputs)
 
     def __repr__(self):
         return _args_repr(self)
@@ -169,3 +160,89 @@ class RandAugment(ClassificationJitter):
             h = vti.torch_to_pil(h.permute(1, 2, 0).cpu())
             h = self.pil_randaugment(h)
             return vti.pil_to_torch(h).to(dtype=x.dtype, device=x.device).permute(2, 0, 1) / 256
+
+
+class PertModelWrapper:
+    def __init__(self, pert_model, init):
+        self.pert_model = pert_model
+        self.init = init
+
+    def __call__(self, inputs):
+        (x, y), put_back = pick_out(lambda a: isinstance(a, (dt.ArraySpatial2D, dt.ClassLabel)),
+                                    inputs)
+
+        x_ = x.unsqueeze(0)
+
+        with torch.no_grad():
+            if not vm.is_built(self.pert_model):
+                self.pert_model(x_)
+                for p in self.pert_model.parameters():
+                    p.requires_grad = False
+            self.init(self.pert_model, x_)
+
+        if isinstance(y, dt.ClassLabel):
+            x = self.pert_model(x_).squeeze(0)
+        else:
+            x_, y_ = self.pert_model(x_, y.unsqueeze(0))
+            x = x_.squeeze(0)
+            y = y_.squeeze(0)
+
+        return put_back((x, y))
+
+
+from vidlu.training.robustness import perturbation as pert
+import vidlu.modules.utils as vmu
+
+
+class PhTPS20(PertModelWrapper):
+    def __init__(self):
+        pert_model = pert.PhotoTPS20()
+        init = vmu.MultiInit(
+            dict(tps=vmui.NormalInit({'offsets': (0, 0.1)}),
+                 photometric=vmui.UniformInit(
+                     {'add_v': [-0.25, 0.25],
+                      'mul_s': [0.25, 2.],
+                      'add_h': [-0.1, 0.1],
+                      'mul_v': [0.25, 2.]})))
+        super().__init__(pert_model, init)
+
+
+class TPS20(PertModelWrapper):
+    def __init__(self):
+        super().__init__(vmi.BackwardTPSWarp(label_interpolation_mode='nearest',
+                                             label_padding_mode=-1),
+                         vmu.NormalInit({'offsets': (0, 0.1)}))
+
+
+class Ph20(PertModelWrapper):
+    def __init__(self):
+        pert_model = pert.Photometric20(forward_arg_count=1)
+        init = vmu.UniformInit(
+            {'add_v': [-0.25, 0.25],
+             'mul_s': [0.25, 2.],
+             'add_h': [-0.1, 0.1],
+             'mul_v': [0.25, 2.]})
+        super().__init__(pert_model, init)
+
+
+class Photometric3(PertModelWrapper):
+    def __init__(self):
+        pert_model = pert.Photometric3(clamp=True)
+        init = vmu.UniformInit(
+            {'add_h.addend': [-1 / 6, 1 / 6],
+             'add_s.addend': [-0.2, 0.2],
+             'add_v.addend': [-0.1, 0.1],
+             'mul_v.factor': [0.25, 2.]})
+        super().__init__(pert_model, init)
+
+
+class ColorJitter:
+    def __init__(self, brightness: T.Union[float, T.Tuple[float, float]] = 0,
+                 contrast: T.Union[float, T.Tuple[float, float]] = 0,
+                 saturation: T.Union[float, T.Tuple[float, float]] = 0,
+                 hue: T.Union[float, T.Tuple[float, float]] = 0):
+        self.pert = tvt.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation,
+                                    hue=hue)
+
+    def __call__(self, inputs):
+        return [self.pert(x) if isinstance(x, dt.Image) else x for x in inputs]
